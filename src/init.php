2b8ec3c219a90e118ab259f289c034623bdd331c
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-01 09:43:50 -0300

    Add a exit to test

--- src/init.php
+++ src/init.php
@@ -4,7 +4,8 @@
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 define('METHOD', $method);
 
-echo METHOD;
+var_dump(METHOD);
+exit;
 
 // define the scheme (http, https)
 $scheme = (isset($_SERVER['HTTPS']) && 'on' === $_SERVER['HTTPS'])? 'https' : 'http';
